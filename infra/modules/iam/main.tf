008d712c6ccc5a013704c8a751c18875cdf287a5
lobobranco96 <rntt2511@gmail.com>
2025-06-11 13:37:13 +0000

    fix lakehouse_dag, delta jars, iam roles. feat: cluster_create_dag para testes, raw_data/2025-06-11

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -5,20 +5,19 @@ resource "google_service_account" "data_pipeline_sa" {
 
 locals {
   roles = [
-    "roles/compute.instanceAdmin.v1",
-    "roles/dataproc.worker",
-    "roles/dataproc.admin",        
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
-    "roles/storage.objectAdmin",
-    "roles/storage.objectViewer",
-    "roles/composer.admin",
-    "roles/bigquery.admin",
-    "roles/composer.worker",
-    "roles/iam.serviceAccountUser",
-    "roles/storage.admin"
+    "roles/dataproc.admin",          # Administra clusters e jobs
+    "roles/dataproc.worker",         # Execução de jobs nos workers
+    "roles/storage.admin",           # Acesso completo a buckets e objetos
+    "roles/bigquery.admin",          # Acesso total a datasets e jobs
+    "roles/composer.admin",          # Criação e gerenciamento de ambientes Composer
+    "roles/composer.worker",         # Execução de DAGs no ambiente
+    "roles/logging.logWriter",       # Gravação de logs
+    "roles/monitoring.metricWriter", # Escrita de métricas no Cloud Monitoring
+    "roles/iam.serviceAccountUser",   # Permite DAGs usarem outras service accounts
+    "roles/compute.instanceAdmin.v1"
   ]
 }
+
 resource "google_project_iam_member" "iam_bindings" {
   for_each = toset(local.roles)
 
